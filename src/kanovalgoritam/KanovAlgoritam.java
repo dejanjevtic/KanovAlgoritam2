7ae4d5aa94a3d40cf98843d102cd56f12a953ec7
dejanjevtic <dejan_d_jevtic@yahoo.com>
2021-04-29 10:59:43 +0200

    Izbaceni komentari
    Svi nepotrebni komentari su izbaceni

--- src/kanovalgoritam/KanovAlgoritam.java
+++ src/kanovalgoritam/KanovAlgoritam.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package kanovalgoritam;
 import java.util.*; 
   
-// Class to represent a graph 
+
 class Graph { 
-    // No. of vertices 
+ 
     int V; 
-  
-    // An Array of List which contains 
-    // references to the Adjacency List of 
-    // each vertex 
+
     List<Integer> adj[]; 
-    // Constructor 
+   
     public Graph(int vertices) 
     { 
         this.V = vertices; 
@@ -30,21 +23,14 @@ class Graph {
     { 
         adj[u].add(v); 
     } 
-    // prints a Topological Sort of the 
-    // complete graph 
+  
     public void topologicalSort() 
     { 
-        // Create a array to store 
-        // indegrees of all 
-        // vertices. Initialize all 
-        // indegrees as 0. 
+    
         int indegree[] = new int[V]; 
            System.out.println(Arrays.toString(indegree)); 
                 
-        // Traverse adjacency lists 
-        // to fill indegrees of 
-        // vertices. This step takes 
-        // O(V+E) time 
+   
         for (int i = 0; i < V; i++) { 
             ArrayList<Integer> temp 
                 = (ArrayList<Integer>)adj[i]; 
@@ -57,8 +43,6 @@ class Graph {
                 
         } 
   
-        // Create a queue and enqueue 
-        // all vertices with indegree 0 
         Queue<Integer> q 
             = new LinkedList<Integer>();  
         
@@ -69,58 +53,42 @@ class Graph {
         System.out.println();
         System.out.println();
         System.out.println(q);
-        // Initialize count of visited vertices 
+     
         int cnt = 0; 
   
-        // Create a vector to store result 
-        // (A topological ordering of the vertices) 
         Vector<Integer> topOrder = new Vector<Integer>();  
         while (!q.isEmpty()) { 
-            // Extract front of queue 
-            // (or perform dequeue) 
-            // and add it to topological order 
+        
             int u = q.poll(); 
             topOrder.add(u); 
   
-            // Iterate through all its 
-            // neighbouring nodes 
-            // of dequeued node u and 
-            // decrease their in-degree 
-            // by 1 
+       
             for (int node : adj[u]) { 
-                // If in-degree becomes zero, 
-                // add it to queue 
+               
                 if (--indegree[node] == 0) 
                     q.add(node); 
             } 
             
             cnt++; 
         } 
-        //System.out.println(topOrder); System.exit(0);
-        // Check if there was a cycle 
+       
         if (cnt != V) { 
             System.out.println( 
                 "There exists a cycle in the graph"); 
             return; 
         } 
-  
-        // Print topological order 
+   
         for (int i : topOrder) { 
             System.out.print(i + " "); 
         } 
     } 
 } 
-/**
- *
- * @author dejan
- */
+
 public class KanovAlgoritam {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+      
         Graph g = new Graph(6); 
         System.out.println(Arrays.toString(g.adj)); 
         
